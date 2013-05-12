787b659fb7ba43642bc4d63c749f28d0cb52ec85
unknown <Administrador@shermanoide.(none)>
2013-05-12 01:54:21 -0300

    eliminando componente viejo

---
+++ ar.apio.main/NewZendModel.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * NewZendModel
+ *  
+ * @author Administrador
+ * @version 
+ */
+
+require_once 'Zend/Db/Table/Abstract.php';
+class NewZendModel extends Zend_Db_Table_Abstract {
+	/**
+	 * The default table name
+	 */
+	protected $_name = 'newzend';
+}
---
+++ ar.apio.main/index.php
@@ -0,0 +1,7 @@
+<?php
+echo '<html>
+	  	<head></head>
+		<body>
+			<H2>ESTO ES UNA PRUEBA DE CONEXION</H2>
+		</body>
+	  <html> ';
